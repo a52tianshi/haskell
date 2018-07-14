50f54c5a84633ad6b548641c6053862a5a067cc1
cq <475215536@qq.com>
2018-07-14 12:29:07 +0800

    page 6

---
+++ chapter1/main.hs
@@ -0,0 +1,22 @@
+module Main where
+
+main :: IO()
+main = do
+print "Hello"
+
+
+print(2 + 15)
+print(5 * 100)
+
+print(True && False)
+
+
+print(5 /= 4)
+
+print(min 9 7)
+
+print(92 `div` 10)
+
+let doubleMe x = x + x
+
+print(doubleMe 45)
