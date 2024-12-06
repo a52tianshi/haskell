4449d5bdc22e182b2940b9148bc1c1efe1395c52
Penny Hardaway <475215536@qq.com>
2024-12-07 02:36:57 +0800

    add Collatz

--- chapter5/Func.hs
+++ chapter5/Func.hs
@@ -1,4 +1,22 @@
-module Func(devideByTen) where 
+module Func(devideByTen,applyTwice,zipWith',chain) where 
 
 devideByTen :: (Floating a) => a -> a
-devideByTen a = a / 10
+devideByTen = (/10)
+
+applyTwice :: (a -> a) -> a -> a
+applyTwice f x = f (f x)
+
+zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWith' _ [] _ = []
+zipWith' _ _ [] = []
+zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
+
+--Collatz sequence
+-- 从一个数开始，如果它是偶数，那么下一个数就是它除以 2，如果它是奇数，那么下一个数就是 3 乘以它再加 1。然后对下一个数重复这个过程。
+-- 通过这样的方式，我们会得到一个序列，它以 1 结尾。这个序列被称为考拉兹序列。
+
+chain :: (Integral a) => a -> [a]
+chain 1 = [1]
+chain n
+    | even n = n:chain (n `div` 2)
+    | odd n = n:chain (n*3 + 1)
--- chapter5/main.hs
+++ chapter5/main.hs
@@ -10,3 +10,25 @@ main = do
 
     print (devideByTen 200)
     print (devideByTen 999)
+    print (applyTwice (+3) 10)
+    print (applyTwice devideByTen 1234) -- applyTwice devideByTen 1234 =  (devideByTen (devideByTen 1234)) 太强了
+
+    print (zipWith' (+) [1,2,3] [4,5,6])
+    print (zipWith' max [1,2,3] [4,5,6])
+
+    print (map (+3) [1,5,3,1,6])
+
+    print (filter (>3) [1,5,3,2,1,6,4,3,2,1])
+
+    print (filter (<15) (filter even [1..20]))
+
+    -- 用 takeWhile 来取得小于 10000 的所有奇数的立方和
+    print (sum (takeWhile (<10000) (filter odd (map (^3) [1..]))))
+    print (sum (takeWhile (<10000) [x^3 | x <- [1..], odd x]))
+
+    -- Collatz sequence
+    print (chain 10)
+    print (chain 1)
+
+    --lambda 就是一次性的匿名函数
+
